39705e64cd22e48695afc00aa053ac3efd2bbad0
BISHA\PC-SYSTEMS <erjonberisha@hotmail.com>
2020-08-18 14:45:44 +0200

    Show TV Show working!

--- showuser.php
+++ showuser.php
@@ -8,9 +8,15 @@ if(isset($_GET['show'])){
     $result = $mysqli->query("SELECT * FROM `shows` WHERE id='$show_id'") or die($mysqli->error);
 }
 ?>
+<style>
+/* body{
+    background-image:url('assets/images/simpsons2.jpg'); background-repeat:no repeat; height: 100%; background-size: 100% 100%; object-fit:cover; 
+} */
 
-<section>
-<section style="padding: 50px 60px; background-image:url('assets/images/simpsons2.jpg'); background-repeat:no repeat; height: 629px; background-size: 100% 100%; " >
+body{
+    background-color: #4e94a4
+}
+</style>
 
 <div style="color:white;" class="container">
 
@@ -24,12 +30,15 @@ while ($row = $result->fetch_assoc()): ?>
 <div class="row justify-content-center">
 <h2 style="font-size:45px; letter-spacing: 4px; text-shadow: 2px 2px 5px blue;">TV SHOW</h2>
 </div>
+        <iframe class="embed-responsive embed-responsive-21by9" style=" width:500px; height:300px;" src="<?php echo $row['src']; ?>"></iframe>
+
         <tr>
             <h3>ID: <?php echo $row['id']; ?></h3>
             <h3>Title: <?php echo $row['title']; ?></h3>
             <h3>Network: <?php echo $row['vendi']; ?></h3>
             <h3>Date: <?php echo $row['date']; ?></h3>
-            <h3>Description: <?php echo $row['description']; ?></h3>
+            <h3>Description: <?php echo $row['description']; ?></h3>            
+            </video>
         </td>
         <?php endwhile ?>   
 </div>

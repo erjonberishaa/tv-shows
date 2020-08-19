a2c0145a04aeea115aa6d9f714b792a0a371da1b
BISHA\PC-SYSTEMS <erjonberisha@hotmail.com>
2020-08-19 15:10:22 +0200

    Navbar Changes

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,18 +12,41 @@
 
 <body>
 <nav class="navbar navbar-expand-md bg-info navbar-dark ">
-  <ul style="margin-left:210px;" class="navbar-nav">
+  <ul style="margin-left:100px;" class="navbar-nav">
   <a href="home.php" class="navbar-brand">
-    <img src="assets/images/tv.png" width="45" alt="" class="d-inline-block align-middle mr-2">
+    <img src="assets/images/movie.png" width="45" alt="" class="d-inline-block align-middle mr-2">
   </a>
 
+<style>
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  border-bottom: 3px solid transparent;
+}
+
+/* .topnav a:hover {
+  border-bottom: 3px solid blue;
+}
+
+.topnav a.active {
+  border-bottom: 3px solid red;
+} */
+
+</style>
+
+<div class="topnav">
   <div style="padding-top:5px;" class="row">
   <a href="home.php" class="navbar-brand">
-    <span class="text-uppercase font-weight-bold">HOME</span>
+    <span class="text-uppercase font-weight-bold" >HOME</span>
   </a>
 
   <a href="suggestions.php" class="navbar-brand">
-    <span class="text-uppercase font-weight-bold">TV Shows Suggestions</span>    
+    <span class="text-uppercase font-weight-bold" style="box-sizing: border-box;">TV Shows Suggestions</span>    
       </a>     
 
       <a href="quotes.php" class="navbar-brand">
@@ -37,7 +60,7 @@
     <span class="text-uppercase font-weight-bold">Movies Quotes</span>    
       </a>  
 
-      </a> 
+      </a>
       </div>
       </div>
 

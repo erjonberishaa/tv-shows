4f6aee3733fb167bb7ccf9dd9d04d427940e3e1b
BISHA\PC-SYSTEMS <erjonberisha@hotmail.com>
2020-08-21 10:22:35 +0200

    Footer Creation!

---
+++ includes/footer.php
@@ -0,0 +1,101 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Latest Movies Website</h5>
+        <p>Get some popcorns and spend your time well watching movies in this website!</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Best other free movies Websites</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="https://azm.to/">AZ Movies</a>
+          </li>
+          <li>
+            <a href="https://www1.solarmovies.co/movie/bloodshot-100550/1-1/watching.html">Solar Movie</a>
+          </li>
+          <li>
+            <a href="https://moviestars.to/">Movie Stars</a>
+          </li>
+          <li>
+            <a href="https://www1.moviesjoy.net/">Movies Joy</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Best Movie Review Websites</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="https://www.rogerebert.com/">Roger Ebert</a>
+          </li>
+          <li>
+            <a href="https://www.rottentomatoes.com/">Rotten Tomatoes</a>
+          </li>
+          <li>
+            <a href="https://www.imdb.com/">IMDb</a>
+          </li>
+          <li>
+            <a href="https://www.metacritic.com/">Meta Critic</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="#!"> latestmovies.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
